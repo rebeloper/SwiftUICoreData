ce426f2defe1e89fc87cfb0adfd4a50e8a518c5e
Alex Nagy <therebeloper@gmail.com>
2021-03-26 10:20:53 +0200

    Initial Commit

---
+++ SwiftUICoreData/SwiftUICoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICoreDataApp.swift
+//  SwiftUICoreData
+//
+//  Created by Alex Nagy on 26.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
