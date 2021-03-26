daf3da791365e92f77e11639ea0d270e90d02258
Alex Nagy <therebeloper@gmail.com>
2021-03-26 11:31:48 +0200

    final

--- SwiftUICoreData/ContentView.swift
+++ SwiftUICoreData/ContentView.swift
@@ -8,9 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Environment(\.managedObjectContext) var managedObjectContext
+    
+    @FetchRequest(
+        entity: Item.entity(),
+        sortDescriptors: [NSSortDescriptor(keyPath: \Item.name, ascending: true)],
+        predicate: NSPredicate(format: "name == %@", "Shirt")
+    ) var items: FetchedResults<Item>
+    
+    @FetchRequest(
+        entity: Category.entity(),
+        sortDescriptors: [NSSortDescriptor(keyPath: \Category.name, ascending: true)]
+    ) var categories: FetchedResults<Category>
+    
+    @State private var isActionSheetPresented = false
+    @State private var isAlertPresented = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Button(action: {
+                let category = Category(context: managedObjectContext)
+                category.name = "Clothes"
+                PersistenceController.shared.save()
+            }, label: {
+                Text("Add Category")
+            })
+            
+            Button(action: {
+                if categories.count == 0 {
+                    isAlertPresented.toggle()
+                } else {
+                    isActionSheetPresented.toggle()
+                }
+            }, label: {
+                Text("Add Item")
+            })
+            .actionSheet(isPresented: $isActionSheetPresented, content: {
+                var buttons = [ActionSheet.Button]()
+                categories.forEach { (category) in
+                    let button = ActionSheet.Button.default(Text("\(category.name ?? "Unknown")")) {
+                        let item = Item(context: managedObjectContext)
+                        item.name = "Shirt"
+                        item.toCategory = category
+                        PersistenceController.shared.save()
+                    }
+                    buttons.append(button)
+                }
+                buttons.append(.cancel())
+                return ActionSheet(title: Text("Please select a category"), message: nil, buttons: buttons)
+            })
+            .alert(isPresented: $isAlertPresented, content: {
+                Alert(title: Text("Please add a category"), message: nil, dismissButton: .cancel(Text("Ok")))
+            })
+            
+            List {
+                ForEach(items, id:\.self) { item in
+                    Text("\(item.name ?? "Unknown") - \(item.toCategory?.name ?? "Unknown")")
+                }
+                .onDelete(perform: removeItem)
+            }
+        }
+    }
+    
+    func removeItem(at offsets: IndexSet) {
+        for index in offsets {
+            let item = items[index]
+            PersistenceController.shared.delete(item)
+        }
     }
 }
 
--- SwiftUICoreData/SwiftUICoreDataApp.swift
+++ SwiftUICoreData/SwiftUICoreDataApp.swift
@@ -9,9 +9,28 @@ import SwiftUI
 
 @main
 struct SwiftUICoreDataApp: App {
+    
+    let persistenceController = PersistenceController.shared
+    
+    @Environment(\.scenePhase) var scenePhase
+        
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+        .onChange(of: scenePhase) { (newScenePhase) in
+            switch newScenePhase {
+            case .background:
+                print("Scene is in background")
+                persistenceController.save()
+            case .inactive:
+                print("Scene is inactive")
+            case .active:
+                print("Scene is active")
+            @unknown default:
+                print("Apple must have changed something")
+            }
         }
     }
 }
